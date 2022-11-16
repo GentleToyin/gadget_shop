9d97ad5cb61e3adfa5b1bb442d5f4f7c1aca3a71
GentleToyin <41758364+GentleToyin@users.noreply.github.com>
2022-11-16 15:28:13 +0100

    Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
 const App = () => {
   return (
     <>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <HomeScreen />
-        </Container>
-      </main>
-      <Footer />
+      <Router>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} exact />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </>
   );
 };
