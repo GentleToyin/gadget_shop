683f3d6bad98916befcbc9e921291979b03a989f
GentleToyin <41758364+GentleToyin@users.noreply.github.com>
2022-11-30 20:30:20 +0100

    api error handler

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,41 @@
+import express from "express";
+import Product from "../models/productModel.js";
+import asyncHandler from "express-async-handler";
+import e from "express";
+
+const router = express.Router();
+
+//@desc Fetch all Products
+// @route GET/api/products
+//@access Public
+// router.get("/", async (req, res) => {
+//   const products = await Product.find({});
+//   res.json(products);
+// });
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+//@desc Fetch single Products
+// @route GET/api/products/:id
+//@access Public
+
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+  })
+);
+
+export default router;
